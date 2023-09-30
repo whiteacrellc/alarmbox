9425e8ef1a99fc5743ea146f0373297859dc1678
tom <tom@tomw.org>
2023-09-30 16:30:05 -0500

    hceckpoint save

--- alarmbox.py
+++ alarmbox.py
@@ -1,4 +1,8 @@
 import daemonize
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from datetime import datetime
 
 class AlarmBox:
     def __init__(self):
@@ -16,6 +20,39 @@ class AlarmBox:
                         name = name.strip()
                         value = value.strip()
                         self.settings[name] = value
+
+    def send_email(self):
+        # Load SendGrid API key
+        date_time = self.get_date_time_string()
+        api_key = self.settings['API_KEY']
+        from_email = self.settings['FROM_EMAIL']
+        to_email = self.settings['TO_EMAIL']
+        email_subject = self.settings.get('EMAIL_SUBJECT', 'Alert From AlarmBox')
+        email_text = self.settings.get('EMAIL_TEXT', f'This is an alert from AlarmBox at {date_time}')
+        # Creaubjte a SendGrid client
+        sg = SendGridAPIClient(api_key)
+
+        # Create a message
+        message = Mail(
+            from_email=from_email, 
+            to_emails=to_email,  
+            subject=email_subject,
+            plain_text_content=email_text
+        )
+
+        # Send the email
+        try:
+            response = sg.send(message)
+            print('Email sent successfully')
+        except Exception as e:
+            print(f'Error: {e}')
+
+    def get_date_time_string(self):
+        # Get the current date and time
+        current_datetime = datetime.now()
+        # Format the datetime object as yyyy-MM-dd HH:mm
+        return current_datetime.strftime('%Y-%m-%d %H:%M')
+
 
 
 def my_daemon_function():
